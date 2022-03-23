5aa3f1abab75ed370066427a63b78ff80444dc3f
passion <passion@pop-os.localdomain>
2022-03-23 20:06:59 +0100

    still not working oops

--- philo/src/routine.c
+++ philo/src/routine.c
@@ -1,10 +1,7 @@
 #include "../include/philo.h"
 
-int	ford_pickup(t_philosophers *philo, t_data *data, int id)
+void	ford_pickup(t_philosophers *philo, t_data *data, int id)
 {
-	(void)philo;
-	if (philo_alive(philo, id))
-		return (1);
 	pthread_mutex_lock(&data->lock[id]);
 	if (data->forks[id] == 0)
 	{
@@ -19,11 +16,6 @@ int	ford_pickup(t_philosophers *philo, t_data *data, int id)
 		print_action(id, FORK, data->start);
 	}
 	pthread_mutex_unlock(&data->lock[(id + 1) % data->size]);
-	return (0);
-}
-
-int	eat_and_drop(t_philosophers *philo, t_data *data, int id)
-{
 	if (data->forks[(id + 1) % data->size] == 1 && data->forks[id] == 1)
 	{
 		print_action(id, EAT, data->start);
@@ -36,15 +28,12 @@ int	eat_and_drop(t_philosophers *philo, t_data *data, int id)
 		pthread_mutex_unlock(&data->lock[(id + 1) % data->size]);
 		philo->nb_eat++;
 	}
-	return (0);
 }
-
-int	philo_alive(t_philosophers *philo, int id)
+/*
+int	eat_and_drop(t_philosophers *philo, t_data *data, int id)
 {
-	if ()
-		return (1);
-	return (0)
-}
+	return (0);
+}*/
 
 void	*routine(void *data)
 {
@@ -54,11 +43,6 @@ void	*routine(void *data)
 	if (philo->id % 2)
 		sleep(200);
 	while (1)
-	{
-		if (ford_pickup(philo, philo->data, philo->id))
-			return ;
-		if (eat_and_drop(philo, philo->data, philo->id))
-			return ;
-	}
+		ford_pickup(philo, philo->data, philo->id);
 	return (NULL);
 }
